5dfd735dca4cdd73c5c6105d6191584893c8c615
yusufbulac <ybulac@gmail.com>
2025-02-04 17:29:40 +0300

    Initial commit: Basic project structure and configurations

---
+++ src/test/java/com/hotelapp/hotel/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelapp.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
